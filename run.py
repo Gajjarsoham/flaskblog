2f41219c9eb19782249ec6124118a6ab056a959b
gajjar Soham <gajjarsoham179@gmail.com>
2026-03-15 13:57:55 +0530

    run.py is updated

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from flaskblog import create_app, db
 
 app = create_app()
 
+# Create database tables
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
