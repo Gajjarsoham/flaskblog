1d6593b3eb58381a7476e1a9e2efb90983ceea9c
gajjar Soham <gajjarsoham179@gmail.com>
2026-03-15 13:23:45 +0530

    updated render deployment

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'site.db')
 
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
